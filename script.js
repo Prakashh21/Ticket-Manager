b94638777e10270ef8d876b3376d5b06f4f88788
Prakashh21 <90280586+Prakashh21@users.noreply.github.com>
2022-02-17 01:49:16 +0530

    Fixed

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let pTag = document.querySelector("p");
-
-// pTag.innerText = "ladsjnd";
-
+let addBtn = document.querySelector(".buttons1");
+let body = document.querySelector("body");
+addBtn.addEventListener("click" , function(){
+let div = document.createElement("div");
+div.classList.add("modal");
+body.append(div);
+}); 
